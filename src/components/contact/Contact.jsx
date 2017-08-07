4a94ef4174b08c6a508d59f8f268c81b242bd00f
Jackson Morgan <jaxoncreed@gmail.com>
2017-08-06 21:27:02 -0700

    Added Jenny contact info

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -9,19 +9,31 @@ export default class Contact extends Component {
       <div className={s.infoPage}>
         <div className={s.contact + ' ' + s.infoContainer}>
           <h1>Reach out to me</h1>
-          <p>If you have a suggestion, want to report a bug, or are having trouble of any kind, don't hesitate to contact me.</p>
+          <p>If you have a suggestion, want to report a bug, or are having trouble of any kind, don't hesitate to contact us.</p>
           <table className="banner-container">
             <tbody>
               <tr>
                 <td><i className="fa fa-twitter"></i></td>
                 <td>
-                  <a href="https://twitter.com/OisForOh" target="_blank">Contact me on twitter: twitter.com/OisForOh</a>
+                  <a href="https://twitter.com/OisForOh" target="_blank">Contact Jackson on twitter: twitter.com/OisForOh</a>
                 </td>
               </tr>
 
               <tr>
                 <td><i className="fa fa-envelope-o"></i></td>
-                <td>You can also email me at jackson.c.morgan@gmail.com</td>
+                <td>You can also email Jackson at jackson.c.morgan@gmail.com</td>
+              </tr>
+
+              <tr>
+                <td><i className="fa fa-twitter"></i></td>
+                <td>
+                  <a href="https://twitter.com/OisForOh" target="_blank">Contact Jenny on twitter: twitter.com/jennywang01</a>
+                </td>
+              </tr>
+
+              <tr>
+                <td><i className="fa fa-envelope-o"></i></td>
+                <td>You can also email Jenny at jennywanggoog@gmail.com</td>
               </tr>
             </tbody>
           </table>
